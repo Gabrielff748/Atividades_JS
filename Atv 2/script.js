d7a6a0b0f7de8139f724c6486cc8b689194fcb38
Gabriel Francisco <133167957+Gabrielff748@users.noreply.github.com>
2024-11-07 18:45:58 -0300

    Add files via upload

--- Atv 2/script.js
+++ Atv 2/script.js
@@ -1,6 +1,6 @@
 function mostrarMensagem(){
     const mensagemDigitada = document.querySelector("#mensagem").value
     const resultado = document.querySelector("#resultado")
-    const mensagem = `Olá ${mensagemDigitada}`
+    const mensagem = `As melhores coisas são as/os ${mensagemDigitada} que conseguimos durante a vida!!`
     resultado.innerHTML = mensagem
 }
